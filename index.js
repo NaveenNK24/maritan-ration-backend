be4665b9e05f0e6119e5c78f5fe37c364e0523e6
Naveentn24 <naveensaven@gmail.com>
2022-09-22 17:30:57 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello Maritan!')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`maritan ration ${port}`)
 })
